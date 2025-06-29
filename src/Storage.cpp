d2678b87f8ce2164e868651b8eb64f3fab7c1b3b
Jeffrey Abreu <jeffreyabreu66@gmail.com>
2025-06-29 14:19:31 -0400

    added to Storage

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -18,8 +18,10 @@ void Storage::save()
 {
     std::ofstream outputFile("tasks.json", std::ios::app); //ios::app ensures that we don't overwrite the file if it alr exists, only append to it
 
-    // Write to the file
-    outputFile.open("tasks.json");
+    if(!outputFile.is_open())
+    {
+        std::cerr << "Error opening the file!"
+    }
 }
 
 void Storage::load()
