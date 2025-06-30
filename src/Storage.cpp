f3c37929441f4e0528b2b2eabf445dc03db12298
Jeffrey Abreu <jeffreyabreu66@gmail.com>
2025-06-30 15:08:48 -0400

    finished load, started add

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -1,8 +1,10 @@
 #include "Storage.hpp"
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <ctime>
 
-explicit Storage::Storage(std::filesystem::path data_file) : file_path(data_file)
+Storage::Storage(std::filesystem::path data_file) : file_path(data_file)
 {
     // check if the path exists, if not generate new one
     if(std::filesystem::exists(file_path))
@@ -15,31 +17,35 @@ explicit Storage::Storage(std::filesystem::path data_file) : file_path(data_file
 Storage::~Storage()
 {
     save();
-    tasks.clear();
+    // tasks.clear(); --> This line isn't necessary since the destructor automatically does this when called
+}
+
+
+int Storage::add(std::string title)
+{
+    nextID++;
+    auto time = std::chrono::system_clock::now();
+    std::string timestamp = std::format("{:%Y-%m-%d %H:%M:%S}", now); 
+    Task t = Task(nextID, title, false, timestamp);
+    tasks.push_back(t);
 }
 
 //Goal of this function should be to read from the tasks vector and write those to the json file, in json format
 
 void Storage::save()
 {
-    std::ofstream outputFile("tasks.json", std::ios::app); //ios::app ensures that we don't overwrite the file if it alr exists, only append to it
+    std::ofstream outputFile(file_path, std::ios::trunc); //ios::trunc ensures that we overwrite the file
 
     if(!outputFile.is_open())
     {
-        std::cerr << "Error opening the file!"
-        return 1;
-    }
-
-    if(task.empty())
-    {
-        std::cout << "There are no tasks to save. " << endl;
+        std::cerr << "Error opening the file!";
         return;
     }
 
-    for(Task t: tasks)
-    {
-        jsonto_json(json &j, const Task &t);
-    }
+
+    json j = tasks;
+
+    outputFile << j.dump() << std::endl; 
 }
 
 void Storage::load()
@@ -51,6 +57,10 @@ void Storage::load()
         std::cerr << "Error opening the file!"
         return 1;
     }
+
+    outputFile >> j;
+
+    tasks = j;
 }
 
 
--- src/Storage.hpp
+++ src/Storage.hpp
@@ -5,13 +5,17 @@
 #include <vector>
 #include "Task.hpp"
 #include <nlohmann/json.hpp>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <format>
 
 
 class Storage{
     explicit Storage(std::filesystem::path data_file);
     ~Storage();
     const std::vector<Task>& list() const;
-    int add(std::string Title, std::string timestamp);
+    int add(std::string title, std::string timestamp);
     //bool mark_done(int id);
     bool remove(int id);
     void save();
@@ -19,6 +23,7 @@ class Storage{
 
     private:
         vector<Task> tasks;
+        int nextID = -1;
         std::filesystem::path file_path;
 };
 
